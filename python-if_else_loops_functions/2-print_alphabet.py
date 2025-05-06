7636fae9e16c12a430cb1156762aea65dcc62e6c
GuarickGit <10615@holbertonstudents.com>
2025-05-06 19:07:15 +0200

    Update 2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
+# Boucle de 'a' à 'z' (97 à 123 en ASCII).
 for letter in range(97, 123):
+    # Print le character actuel dans letter, sans saut à la ligne.
     print("{}".format(chr(letter)), end="")
