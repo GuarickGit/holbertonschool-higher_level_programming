b1b74cc34a8a0e0f70796b664b9b96de27dedaa2
GuarickGit <10615@holbertonstudents.com>
2025-05-06 19:05:45 +0200

    Update 3-print_alphabt.py | add comments

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# Boucle de 'a' à 'z' (97 à 123 en ASCII).
 for letter in range(97, 123):
+    # Si letter n'est pas égale à 'e' ET 'q'.
     if letter != 101 and letter != 113:
+        # Print le character actuel dans letter, sans saut à la ligne.
+        # chr(letter) pour ASCII | end="" pour pas de retour à la ligne.
         print("{}".format(chr(letter)), end="")
