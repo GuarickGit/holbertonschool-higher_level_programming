7aca274137237021413923626d37f12ac123d0da
GuarickGit <10615@holbertonstudents.com>
2025-05-06 18:35:27 +0200

    Update 4-print_hexa.py | Add comments

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
+# Boucle de 0 à 98
 for number in range(0, 99):
+    # Print Number en Décimal, "=", puis 0x"HEX" (équivalence HEXA en ASCII)
     print("{} = ".format(number) + hex(number))
