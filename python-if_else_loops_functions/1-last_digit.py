4f4bb3c5630bc0b0d0c50fe0d056a1ae6b0cd5db
GuarickGit <10615@holbertonstudents.com>
2025-05-06 19:22:27 +0200

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+# Importe le moule 'random'
 import random
+# number contient un chiffre aléatoire entre -10000 et 10000 à chaque run.
 number = random.randint(-10000, 10000)
+# last_digit contient les deux last digit absolu (abs) de number.
 last_digit = abs(number) % 10
 
+# Si number inférieur à zéro, alors last_digit devient -last_digit (négatif)
 if number < 0:
     last_digit = -last_digit
 
