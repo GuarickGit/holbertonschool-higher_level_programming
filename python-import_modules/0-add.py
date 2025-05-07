e58a1fa8b00eb008738ca260b29b380a022f2e71
GuarickGit <10615@holbertonstudents.com>
2025-05-07 10:43:08 +0200

    Update 0-add.py | add comments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+# Importe la fonction add depuis le module add_0
 from add_0 import add
-a = 1
-b = 2
+a = 1 # Déclare et initialise la variable a avec la valeur 1
+b = 2 # Déclare et initialise la variable b avec la valeur 2
 
-
+# Vérifie si ce script est exécuté directement (et non importé comme module)
 if __name__ == "__main__":
+    # Affiche le résultat de l'addition de a et b en utilisant la fonction add
     print("{} + {} = {}".format(a, b, add(a, b)))
