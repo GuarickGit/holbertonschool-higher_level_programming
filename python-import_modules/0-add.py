2c65fdeba82247f2ff10f7d55d8150c3b6502be3
GuarickGit <10615@holbertonstudents.com>
2025-05-07 10:44:40 +0200

    Update 0-add.py | add comments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 
 # Importe la fonction add depuis le module add_0
 from add_0 import add
-a = 1 # Déclare et initialise la variable a avec la valeur 1
-b = 2 # Déclare et initialise la variable b avec la valeur 2
+a = 1  # Déclare et initialise la variable a avec la valeur 1
+b = 2  # Déclare et initialise la variable b avec la valeur 2
 
 # Vérifie si ce script est exécuté directement (et non importé comme module)
 if __name__ == "__main__":
