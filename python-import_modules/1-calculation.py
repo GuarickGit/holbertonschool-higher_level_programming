05290b2005d8705b84d72ab0ec10ae7e098c6886
GuarickGit <10615@holbertonstudents.com>
2025-05-07 10:46:02 +0200

    Update 1-calculation.py | Add comments

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,8 +2,8 @@
 
 # Importe les fonctions add, sub, mul et div depuis le module calculator_1
 from calculator_1 import add, sub, mul, div
-a = 10 # Initialise la variable a avec la valeur 10
-b = 5 # Initialise la variable b avec la valeur 5
+a = 10  # Initialise la variable a avec la valeur 10
+b = 5  # Initialise la variable b avec la valeur 5
 
 # Exécute ce bloc uniquement si le script est lancé directement
 if __name__ == "__main__":
