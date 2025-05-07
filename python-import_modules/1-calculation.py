e97bbba3ddedb9fc832deb14a0da6396cd7260d9
GuarickGit <10615@holbertonstudents.com>
2025-05-07 11:23:46 +0200

    Update 1-calculation.py | Delete comments for Pycode

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
-# Importe les fonctions add, sub, mul et div depuis le module calculator_1
 from calculator_1 import add, sub, mul, div
-a = 10  # Initialise la variable a avec la valeur 10
-b = 5  # Initialise la variable b avec la valeur 5
+a = 10
+b = 5
 
-# Exécute ce bloc uniquement si le script est lancé directement
 if __name__ == "__main__":
-    # Affiche le résultat de l'addition
     print("{} + {} = {}".format(a, b, add(a, b)))
-    # Affiche le résultat de la soustraction
     print("{} - {} = {}".format(a, b, sub(a, b)))
-    # Affiche le résultat de la multiplication
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    # Affiche le résultat de la division
     print("{} / {} = {}".format(a, b, div(a, b)))
