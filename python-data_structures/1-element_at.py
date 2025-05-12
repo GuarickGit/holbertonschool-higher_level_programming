63197b96f5501b5c017283b0b91a99c49a550f1f
GuarickGit <10615@holbertonstudents.com>
2025-05-12 10:44:17 +0200

    Add 1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    for number in range(len(my_list)):
+        if idx < 0:
+            return None
+
+        elif idx > len(my_list):
+            return None
+
+        else:
+            return idx + 1
