53813c5da8e57507232564f4d2e96d0a8e9caa22
GuarickGit <10615@holbertonstudents.com>
2025-05-12 10:53:42 +0200

    Update 1-element_at.py | Delete tabs

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-        # Si idx est négatif, return None.
-        if idx < 0:
-            return None
-        # Si idx est supérieur à la range, return None.
-        elif idx > len(my_list) - 1:
-            return None
-        # Sinon, return l'index dans "my_list"
-        else:
-            return my_list[idx]
+    # Si idx est négatif, return None.
+    if idx < 0:
+        return None
+    # Si idx est supérieur à la range, return None.
+    elif idx > len(my_list) - 1:
+        return None
+    # Sinon, return l'index dans "my_list"
+    else:
+        return my_list[idx]
