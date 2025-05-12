b1800f2815803b2d2c3aa363ce0840e0c0441253
GuarickGit <10615@holbertonstudents.com>
2025-05-12 15:00:28 +0200

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
 def new_in_list(my_list, idx, element):
-    new_list = list(my_list)
+    # Crée une copie de my_list.
+    list_copy = list(my_list)
+    # Si idx est négatif, return la copie de la liste d'origine.
     if idx < 0:
-        return my_list
+        return list_copy
+    # Si idx est supérieur à la range, return la copie de la liste d'origine.
     elif idx >= len(my_list):
-        return my_list
+        return list_copy
+    # Sinon, modifie l'élément dans la copie de la liste, puis la return.
     else:
-        new_list[idx] = element
-        return new_list
+        list_copy[idx] = element
+        return list_copy
