34fc93e9c58a57bf14c60fa676ece8e1bcdd19d7
GuarickGit <10615@holbertonstudents.com>
2025-05-12 14:28:26 +0200

    Add 3-print_reversed_list.py

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def print_reversed_list_integer(my_list=[]):
+    # Boucle sur my_list, en partant de la fin.
+    for number in reversed(my_list):
+        # Print number à chaque itération.
+        print("{:d}".format(number))
