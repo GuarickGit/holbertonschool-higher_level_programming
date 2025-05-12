9643ff4afaecc1ae51c46ab7b2c649a8f316c5ba
GuarickGit <10615@holbertonstudents.com>
2025-05-12 15:41:49 +0200

    Update 6-print_matrix_integer.py | add comments

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    # Boucle sur les lignes de matrix.
     for row in matrix:
+        # Boucle sur les caractères de la ligne.
         for i in range(len(row)):
+            # Si i est égale à la length de row - 1.
             if i == len(row) - 1:
+                # Print le caractère actuel de la ligne, sans espace.
                 print("{:d}".format(row[i]), end="")
             else:
+                # Print le caractère actuel de la ligne, avec espace.
                 print("{:d}".format(row[i]), end=" ")
+        # Print un retour à la ligne.
         print()
