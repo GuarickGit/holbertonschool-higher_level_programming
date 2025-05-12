5eb9915dc491a79fbc7368db1654a546d652d37b
GuarickGit <10615@holbertonstudents.com>
2025-05-12 10:29:52 +0200

    Update 0-print_list_integer.py | Add specifier {:d} for str.format()

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@ def print_list_integer(my_list=[]):
     # Boucle sur la length de "my_list" (Index 0 à len -1).
     for number in range(len(my_list)):
         # print chaque index de my_list.
-        print("{}".format(my_list[number]))
+        print("{:d}".format(my_list[number]))
