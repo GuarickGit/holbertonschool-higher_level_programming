4890784668502496752235c0fb28d571a1154ce8
GuarickGit <10615@holbertonstudents.com>
2025-05-13 10:03:50 +0200

    Update 11-delete_at.py | Delete new_list and keep the same list (my_list)

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    new_list = []
     # Si idx est négatif, return la liste sans rien faire.
     if idx < 0:
         return my_list
     # Si idx est supérieur à la range, return la liste sans rien faire.
     elif idx >= len(my_list):
         return my_list
-    # Sinon, on construit une nouvelle liste sans l'élément à cet indice.
+    # Sinon, on supprime l'élément à cet indice, puis on return la liste.
     else:
-        # On construit une nouvelle liste en excluant l'élément à l'indice idx :
-        # my_list[:idx] prend tous les éléments avant idx (non inclus)
-        # my_list[idx + 1:] prend tous les éléments après idx (donc on saute idx)
-        # On les concatène pour obtenir une nouvelle liste sans l'élément ciblé
-        new_list = my_list[:idx] + my_list[idx + 1:]
-        return new_list
+        del my_list[idx]
+        return my_list
