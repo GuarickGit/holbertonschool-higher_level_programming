72b73fc44b9410944bd9f0d4b10ac23443bf58e8
GuarickGit <10615@holbertonstudents.com>
2025-05-13 15:15:05 +0200

    Add 3-common_elements.py

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    # On récupère les éléments communs entre les deux ensembles
+    common = set_1.intersection(set_2)
+
+    # On retourne l'ensemble des éléments communs
+    return common
