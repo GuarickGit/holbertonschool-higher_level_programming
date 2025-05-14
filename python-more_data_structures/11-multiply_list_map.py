cebbe807a42b6378b7179721424ebae53273d0d6
GuarickGit <10615@holbertonstudents.com>
2025-05-14 13:43:16 +0200

    Update 11-multiply_list_map.py | Add comments

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
+    # Utilise map pour appliquer une multipli. à chaque élément de la liste
+    # Lambda prend chaque élément x de my_list et retourne x * number
+    # map retourne un itérable, qu'on transforme en liste avec list()
     return list(map(lambda x: x * number, my_list))
