d00f0ce816716c5024cc26c539b50561bbfcb42d
GuarickGit <10615@holbertonstudents.com>
2025-05-14 11:38:14 +0200

    Update 8-simple_delete.py | Pycode

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,8 @@
 
 def simple_delete(a_dictionary, key=""):
     # Supprime la clé du dictionnaire si elle existe.
-    # Si la clé n'existe pas, ne fait rien (grâce à la valeur par défaut dans .pop()).
+    # Si la clé n'existe pas, ne fait rien
+    # (grâce à la valeur par défaut dans .pop()).
     a_dictionary.pop(key, "missing key")
     # Retourne le dictionnaire mis à jour
     return a_dictionary
