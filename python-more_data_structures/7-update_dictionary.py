aeb78570d42b1c5978a2383b944b5d55f953ad21
GuarickGit <10615@holbertonstudents.com>
2025-05-14 11:14:45 +0200

    Update 7-update_dictionary.py | Add comments

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,5 @@
 def update_dictionary(a_dictionary, key, value):
     # Ajoute ou met à jour la clé avec la valeur donnée
     a_dictionary[key] = value
+    # Return le dictionnaire mis à jour
     return a_dictionary
