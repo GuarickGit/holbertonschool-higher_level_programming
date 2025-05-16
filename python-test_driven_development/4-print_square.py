f6772416b6becdc1519870d15eda500abc4fe467
GuarickGit <10615@holbertonstudents.com>
2025-05-16 16:41:33 +0200

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
     ValueError: If size is negative.
     """
 
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
