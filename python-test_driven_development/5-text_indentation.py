5d2457ea599b81e3bc4a935288c01d3a9d3ef2a2
GuarickGit <10615@holbertonstudents.com>
2025-05-18 11:41:02 +0200

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that prints text with two new lines after '.', '?' and ':' characters."""
+"""Module that prints text with two new lines after '.', '?' and ':'
+characters."""
 
 
 def text_indentation(text):
