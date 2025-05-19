93b45e6c08883a0b71f23fde142a78835743ab25
GuarickGit <10615@holbertonstudents.com>
2025-05-19 10:23:20 +0200

    Add 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Ce module contient une définition vide de la classe Square."""
+
+
+class Square:
+    """Cette classe représente un carré sans fonctionnalités pour le moment."""
+    pass
