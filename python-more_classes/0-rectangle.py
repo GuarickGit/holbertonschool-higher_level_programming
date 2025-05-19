f9de70cad108411f3846543bfe30d21d23e83bf4
GuarickGit <10615@holbertonstudents.com>
2025-05-19 15:27:19 +0200

    Add 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Ce module contient une définition vide de la classe Rectangle."""
+
+
+class Rectangle:
+    """Cette classe représente un rectangle sans fonctionnalités
+    pour le moment."""
+    pass
