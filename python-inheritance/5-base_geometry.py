c6681ef872c62a1fd816c3ca25c59f061607d9d0
GuarickGit <10615@holbertonstudents.com>
2025-05-26 15:14:49 +0200

    update | add docstrings

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Ce module contient la définition d'une classe de base pour des formes
+géométriques.
+"""
+
 
 class BaseGeometry:
+    """
+    Classe de base pour représenter des formes géométriques.
+    Elle sera étendue avec des fonctionnalités spécifiques par la suite.
+    """
     pass
