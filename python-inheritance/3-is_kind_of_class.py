6c42a152366da3491c87c9b95594be2aaeb5b25a
GuarickGit <10615@holbertonstudents.com>
2025-05-26 14:32:41 +0200

    Update | pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,18 +3,18 @@
 
 
 def is_kind_of_class(obj, a_class):
-     """
-    Retourne True si obj est une instance de a_class ou d'une classe héritée
-    de a_class.
-
-    Args:
-        obj: L'objet à vérifier.
-        a_class: La classe de référence.
-
-    Returns:
-        True si obj est une instance ou hérite de a_class, sinon False.
     """
-     if isinstance(obj, a_class):
+   Retourne True si obj est une instance de a_class ou d'une classe héritée
+   de a_class.
+
+   Args:
+       obj: L'objet à vérifier.
+       a_class: La classe de référence.
+
+   Returns:
+       True si obj est une instance ou hérite de a_class, sinon False.
+   """
+    if isinstance(obj, a_class):
         return True
-     else:
+    else:
         return False
