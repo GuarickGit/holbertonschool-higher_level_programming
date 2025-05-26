c665127a6f0b5fed11b6fbf6dfd17aaf921dce9d
GuarickGit <10615@holbertonstudents.com>
2025-05-26 11:33:14 +0200

    Update | Remove docstrings for checker

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 """
-Ce module fournit une fonction pour vérifier si un objet est exactement une
-instance d'une classe donnée, sans prendre en compte l'héritage.
+xxx
 """
 
 
 def is_same_class(obj, a_class):
     """
-    Return True si l'objet est exactement une instance de la classe spécifiée.
-
-    Cette fonction vérifie si le type de "obj" est strictement égal à
-    "a_class", sans prendre en compte l'héritage.
-
-    Arguments :
-        obj : l'objet à tester.
-        a_class : la classe avec laquelle comparer.
-
-    Return :
-        True si "obj" est exactement une instance de "a_class", sinon False.
+    xxx
     """
     if type(obj) == a_class:
         return True
