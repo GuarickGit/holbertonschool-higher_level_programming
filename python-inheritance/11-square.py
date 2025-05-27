4bec77bfbf61052ff3ed6cd01cff2035d488170c
GuarickGit <10615@holbertonstudents.com>
2025-05-27 13:40:26 +0200

    add 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Ce module contient la définition d'une classe de carré,
+qui hérite de la classe Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Classe représentant un carré, hérite de Rectangle et utilise une seule
+    dimension pour définir la largeur et la hauteur.
+    """
+
+    def __init__(self, size):
+        """
+        Initialise une nouvelle instance de Square avec une taille donnée.
+        La taille est validée comme un entier positif.
+        """
+        self.integer_validator("size", size)
+        # Appelle le constructeur de Rectangle avec size pour la largeur
+        # et la hauteur car un carré est un rectangle avec des côtés égaux
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calcule et retourne l’aire du carré.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Retourne une représentation lisible du carré.
+
+        Returns:
+            str: La description du carré au format:
+            [Square] <size>/<size>.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
