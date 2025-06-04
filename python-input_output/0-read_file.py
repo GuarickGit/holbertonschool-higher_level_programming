057673f9a10eab16aa121f411b8fa37996eeda73
GuarickGit <10615@holbertonstudents.com>
2025-06-04 13:15:29 +0200

    Update | Pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,10 +2,10 @@
 """Ce module permet de lire un fichier texte en UTF-8 et d'en afficher le
 contenu sur la sortie standard."""
 
+
 def read_file(filename=""):
     """Lit un fichier texte UTF-8 et affiche son contenu sur la sortie
     standard."""
 
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
