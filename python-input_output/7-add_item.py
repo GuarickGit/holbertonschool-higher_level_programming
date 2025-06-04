ffdf3d8c9031ae2e72a5042f399e9d2b7153f580
GuarickGit <10615@holbertonstudents.com>
2025-06-04 19:24:46 +0200

    Add 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Ce script ajoute tous les arguments passés en ligne de commande
+dans une liste Python, puis les sauvegarde dans un fichier JSON.
+Le fichier utilisé est 'add_item.json'. Il est créé s’il n’existe pas.
+"""
+
+import sys  # Pour récupérer les arguments passés au script
+import os  # Pour vérifier si le fichier existe
+from save_to_json_file import save_to_json_file  # Pour sauvegarder la liste
+from load_from_json_file import load_from_json_file  # Pour charger la liste
+
+# Si le fichier existe, on charge la liste à partir de celui-ci
+if os.path.exists("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+else:
+    # Sinon, on part d'une liste vide
+    my_list = []
+
+# On ajoute les arguments passés au script (en ignorant le nom du script)
+my_list.extend(sys.argv[1:])
+
+# On sauvegarde la nouvelle liste dans le fichier JSON
+save_to_json_file(my_list, "add_item.json")
