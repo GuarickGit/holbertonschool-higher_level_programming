2f2b360266ee529f8d82e6a3cbc1e03dfcc0b076
GuarickGit <10615@holbertonstudents.com>
2025-06-23 14:03:06 +0200

    Add 10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records from second_table ordered by score (top first)
+-- Selecting score and name, ordered by highest score first
+SELECT score, name FROM second_table ORDER BY score DESC, name
