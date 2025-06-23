9ace357ecad862d615b5e0ac72bba9e44786c83a
GuarickGit <10615@holbertonstudents.com>
2025-06-23 11:24:36 +0200

    Update | Add the missing "S" for EXIST(S)

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- create a table called first_table in the current database in my MySQL server
-CREATE TABLE IF EXIST first_table (
+CREATE TABLE IF EXISTS first_table (
 	id INT,
 	name VARCHAR(256),
 );
