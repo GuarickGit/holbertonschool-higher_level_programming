beceb274c61bffa182fa517bc1548d0f78d6b2fd
GuarickGit <10615@holbertonstudents.com>
2025-06-23 15:01:51 +0200

    Add 15-groups.sql

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score in second_table
+-- for each score, sorted by the number of records (descending)
+SELECT COUNT(*) AS number, score
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
