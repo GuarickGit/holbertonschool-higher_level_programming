d8a90465f69419b5a61028a2c8c4a8114570c8db
GuarickGit <10615@holbertonstudents.com>
2025-06-24 10:44:35 +0200

    Update | replace hbtn_0d_2 by *.*

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates the MySQL user 'user_0d_2'@'localhost' with the password 'user_0d_2_pwd'
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Grants select privilege on all databases and tables to 'user_0d_2'@'localhost'
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
