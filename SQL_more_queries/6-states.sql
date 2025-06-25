01874ccdcbba02858868a0d8fcd4ac893b92ff42
GuarickGit <10615@holbertonstudents.com>
2025-06-25 10:19:54 +0200

    Update | Add .states for table creation

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Create the database hbtn_0d_usa if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Create the MySQL table 'states' if it doesn't already exist
-CREATE TABLE IF NOT EXISTS states (
+-- Create the MySQL table 'states', in the database hbtn_0d, if it doesn't already exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
