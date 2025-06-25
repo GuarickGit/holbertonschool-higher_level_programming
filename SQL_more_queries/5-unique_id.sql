828900635349db3af70bb4b2b18c9ab117ad04a2
GuarickGit <10615@holbertonstudents.com>
2025-06-25 10:06:33 +0200

    Update | return to initial state

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- Script that creates the table unique_id on my MySQL server
 -- id value is 1 by default and is unique
 CREATE TABLE IF NOT EXISTS unique_id (
-	ID INT NOT NULL DEFAULT 1,
+	id INT DEFAULT 1,
 	name VARCHAR(256),
-	UNIQUE (ID)
+	UNIQUE (id)
 );
