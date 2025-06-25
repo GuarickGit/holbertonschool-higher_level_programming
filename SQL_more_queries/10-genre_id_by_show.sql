bf5c099014bc3f53fe1a1838e7d0ab14c664f82c
GuarickGit <10615@holbertonstudents.com>
2025-06-25 15:39:24 +0200

    Update | Remove s from tv_show(s)  line 4

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all shows in the database that have at least one genre linked
 -- Select shows that have at least one genre, with title and genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
+FROM tv_show
 INNER JOIN tv_show_genres ON tv_show_genres.tv_show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
