759ecca2da542e0a22d9518739777ed27994b26e
GuarickGit <10615@holbertonstudents.com>
2025-06-25 09:58:39 +0200

    Update | Caps Lock ID

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,7 @@
 -- Script that creates the table unique_id on my MySQL server
 -- id value is 1 by default and is unique
 CREATE TABLE IF NOT EXISTS unique_id (
-	id INT DEFAULT 1,
+	ID INT DEFAULT 1,
 	name VARCHAR(256)
-	UNIQUE (id)
+	UNIQUE (ID)
 );
