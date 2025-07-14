ea660c6970cc9cb7a48484bfa058d94ba5217b8a
GuarickGit <10615@holbertonstudents.com>
2025-07-14 13:05:40 +0200

    Update | Semistandard fixed

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -5,11 +5,9 @@ const headerElement = document.querySelector('header');
 
 element.addEventListener('click', function () {
   if (headerElement.classList.contains('red')) {
-  headerElement.classList.remove('red');
-  headerElement.classList.add('green');
-  }
-
-  else if (headerElement.classList.contains('green')) {
+    headerElement.classList.remove('red');
+    headerElement.classList.add('green');
+  } else if (headerElement.classList.contains('green')) {
     headerElement.classList.remove('green');
     headerElement.classList.add('red');
   }
