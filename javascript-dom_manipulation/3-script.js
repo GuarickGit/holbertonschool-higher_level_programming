65d9f552952a6dc848af4ce0b0fcbac688fd52ab
GuarickGit <10615@holbertonstudents.com>
2025-07-14 13:03:10 +0200

    Add 3-script.js

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const element = document.getElementById('toggle_header');
+const headerElement = document.querySelector('header');
+
+element.addEventListener('click', function () {
+  if (headerElement.classList.contains('red')) {
+  headerElement.classList.remove('red');
+  headerElement.classList.add('green');
+  }
+
+  else if (headerElement.classList.contains('green')) {
+    headerElement.classList.remove('green');
+    headerElement.classList.add('red');
+  }
+});
