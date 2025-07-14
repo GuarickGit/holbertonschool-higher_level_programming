37b43bba4cbb2fccc98fac1f4a8f86e36832b7d8
GuarickGit <10615@holbertonstudents.com>
2025-07-14 15:36:13 +0200

    Add 6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const name = data.name;
+    const character = document.getElementById('character');
+    character.textContent = name;
+  });
