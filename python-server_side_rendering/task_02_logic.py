5119b5e0cbf6b5936cde1ee7db7420acbbcfa151
GuarickGit <10615@holbertonstudents.com>
2025-07-15 14:28:22 +0200

    Add task_02_logic.py

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    # Ouvre le fichier JSON contenant les données (liste d'objets)
+    with open("items.json", "r") as file:
+        data = json.load(file)  # Charge le contenu JSON dans une var. Python
+        # Récupère la liste associée à la clé "items",
+        # ou une liste vide si elle est absente
+        items = data.get("items", [])
+    # Return le template items.html en lui passant la liste d'objets récupérée
+    return render_template('items.html', items=items)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
